1421ae67467e92dbcbd7977aea4ce5a749c95a53
chandhu <chandhulal99@gmail.com>
2026-02-13 20:14:29 +0530

    update

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,3 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 });
-Route::get('/broadcast-test', function () {
-    $reminder = Reminder::first();
-    event(new ReminderSent($reminder));
-    return 'event fired';
-});
